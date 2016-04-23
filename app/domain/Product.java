1073627177e2fec42ef04dfd4bf821830a8778fe
Tylos <grtylos@gmail.com>
2016-04-23 11:21:34 +0200

    Add initial fixture for ships products

--- app/domain/Product.java
+++ app/domain/Product.java
@@ -1,5 +1,18 @@
 package domain;
 
-public class Product {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Product {
+    public Long id;
+    public String name;
+    public String model;
+    public String manufacturer;
+    public String startship_class;
+    public String cost_in_credits;
+    public String image;
+    public String passengers;
+    public String crew;
+    public String cargo_capacity;
+    public String hyperdrive_rating;
 }
--- app/repositories/MemoryProductsRepository.java
+++ app/repositories/MemoryProductsRepository.java
@@ -1,10 +1,12 @@
 package repositories;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Product;
-import repositories.ProductsRepository;
 
 import javax.inject.Singleton;
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,9 +23,25 @@ public class MemoryProductsRepository implements ProductsRepository {
     }
 
     private void fillProductList(Map<Long, Product> products) {
-        for (int i = 0; i < 20; i++) {
-            products.put((long) (i+1), new Product());
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<Product> fixtures = objectMapper.readValue(
+                    new File("./app/assets/product_fixture.json"),
+                    new TypeReference<List<Product>>() {
+            });
+
+            for (int i = 0; i < fixtures.size(); i++) {
+                Product value = fixtures.get(i);
+                long id = (long) (i + 1);
+                value.id = id;
+                products.put(id, value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        //new ObjectMapper().readValue(parse, new TypeReference<List<Product>>(){});
     }
 
     @Override
