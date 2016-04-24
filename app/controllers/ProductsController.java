a00bbe6d44953e8440d229f8a931d849d70434fe
Tylos <grtylos@gmail.com>
2016-04-24 09:40:15 +0200

    Move api controller to it's own package to be more clear

--- app/controllers/ProductsController.java
+++ app/controllers/ProductsController.java
@@ -20,76 +20,17 @@ public class ProductsController extends Controller {
     @Inject
     ProductsRepository productsRepository;
 
-    @Inject
-    UserRepository userRepository;
-
     public Result list() {
-        // TODO Check the headers here and swipe anon
-        User userContext = UserRepository.anonymous;
-
-        return ok(Json.toJson(
-                mapProducts(productsRepository.getAll(), userContext)));
+        return ok(Json.toJson(productsRepository.getAll()));
     }
 
     public Result show(Long id) {
-        // TODO Check the headers here and swipe anon
-        User userContext = UserRepository.anonymous;
-
         Product product = productsRepository.getById(id);
         if (product != null) {
-            return ok(Json.toJson(mapProduct(product, userContext)));
-        } else {
-            return notFound();
-        }
-    }
-
-    @Security.Authenticated(ActionAuthenticator.class)
-    public Result favoriteProduct(Long id) {
-        User user = userRepository.getByIdentifier(ctx().request().username());
-        Product product = productsRepository.getById(id);
-
-        if (user != null && product != null) {
-            user.favorite(product);
             return ok(Json.toJson(product));
         } else {
-            Logger.debug("Product = " + product + "User " + user);
             return notFound();
         }
     }
 
-    @Security.Authenticated(ActionAuthenticator.class)
-    public Result unfavoriteProduct(Long id) {
-        User user = userRepository.getByIdentifier(ctx().request().username());
-        Product product = productsRepository.getById(id);
-        user.unfavorite(product);
-        return ok(Json.toJson(product));
-    }
-
-    private List<ProductView> mapProducts(List<Product> all, User userContext) {
-        List<ProductView> results = new ArrayList<>();
-        all.forEach(product -> {
-            results.add(mapProduct(product, userContext));
-        });
-
-        return results;
-    }
-
-    private ProductView mapProduct(Product product, User userContext) {
-        return ProductView.newBuilder()
-                .withId(product.id)
-                .withName(product.name)
-                .withModel(product.model)
-                .withManufacturer(product.manufacturer)
-                .withStarshipClass(product.starship_class)
-                .withCostInCredits(product.cost_in_credits)
-                .withImage(product.image)
-                .withPassengers(product.passengers)
-                .withCrew(product.crew)
-                .withCargoCapacity(product.cargo_capacity)
-                .withHyperdriveRating(product.hyperdrive_rating)
-                .withIsFeatured(product.isFeatured)
-                .withIsFavorite(userContext.isFavorite(product))
-                .withIsInCart(userContext.isInCart(product))
-                .build();
-    }
 }
---
+++ app/controllers/api/ProductsController.java
@@ -0,0 +1,97 @@
+package controllers.api;
+
+import javax.inject.Inject;
+
+import controllers.ActionAuthenticator;
+import domain.Product;
+import domain.User;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+import repositories.ProductsRepository;
+import repositories.UserRepository;
+import views.ProductView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductsController extends Controller {
+
+    @Inject
+    ProductsRepository productsRepository;
+
+    @Inject
+    UserRepository userRepository;
+
+    public Result list() {
+        // TODO Check the headers here and swipe anon
+        User userContext = UserRepository.anonymous;
+
+        return ok(Json.toJson(
+                mapProducts(productsRepository.getAll(), userContext)));
+    }
+
+    public Result show(Long id) {
+        // TODO Check the headers here and swipe anon
+        User userContext = UserRepository.anonymous;
+
+        Product product = productsRepository.getById(id);
+        if (product != null) {
+            return ok(Json.toJson(mapProduct(product, userContext)));
+        } else {
+            return notFound();
+        }
+    }
+
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result favoriteProduct(Long id) {
+        User user = userRepository.getByIdentifier(ctx().request().username());
+        Product product = productsRepository.getById(id);
+
+        if (user != null && product != null) {
+            user.favorite(product);
+            return ok(Json.toJson(product));
+        } else {
+            Logger.debug("Product = " + product + "User " + user);
+            return notFound();
+        }
+    }
+
+    @Security.Authenticated(ActionAuthenticator.class)
+    public Result unfavoriteProduct(Long id) {
+        User user = userRepository.getByIdentifier(ctx().request().username());
+        Product product = productsRepository.getById(id);
+        user.unfavorite(product);
+        return ok(Json.toJson(product));
+    }
+
+    private List<ProductView> mapProducts(List<Product> all, User userContext) {
+        List<ProductView> results = new ArrayList<>();
+        all.forEach(product -> {
+            results.add(mapProduct(product, userContext));
+        });
+
+        return results;
+    }
+
+    private ProductView mapProduct(Product product, User userContext) {
+        return ProductView.newBuilder()
+                .withId(product.id)
+                .withName(product.name)
+                .withModel(product.model)
+                .withManufacturer(product.manufacturer)
+                .withStarshipClass(product.starship_class)
+                .withCostInCredits(product.cost_in_credits)
+                .withImage(product.image)
+                .withPassengers(product.passengers)
+                .withCrew(product.crew)
+                .withCargoCapacity(product.cargo_capacity)
+                .withHyperdriveRating(product.hyperdrive_rating)
+                .withIsFeatured(product.isFeatured)
+                .withIsFavorite(userContext.isFavorite(product))
+                .withIsInCart(userContext.isInCart(product))
+                .build();
+    }
+}
